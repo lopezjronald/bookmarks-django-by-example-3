ae0019e45bc11ba59bcc864511e33b03d402fcdf
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2021-09-29 13:36:29 -0700

    removed cert.crt and key

--- images/forms.py
+++ images/forms.py
@@ -13,25 +13,25 @@ class ImageCreateForm(forms.ModelForm):
             'url': forms.HiddenInput,
         }
 
-        def clean_url(self):
-            url = self.cleaned_data['url']
-            valid_extensions = ['jpg', 'jpeg']
-            extension = url.rsplit('.', 1)[1].lower()
-            if extension not in valid_extensions:
-                raise forms.ValidationError('The given URL does not match valid image extensions.')
-            return url
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extensions.')
+        return url
 
-        def save(self, force_insert=False, force_update=False, commit=True):
-            image = super().save(commit=False)
-            image_url = self.cleaned_data['url']
-            name = slugify(image.title)
-            extension = image_url.rsplit('.', 1)[1].lower()
-            image_name = f'{name}.{extension}'
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
 
-            # download image from the given URL
-            response = request.urlopen(image_url)
-            image.image.save(image_name, ContentFile(response.read()), save=False)
+        # download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
 
-            if commit:
-                image.save()
-            return image
+        if commit:
+            image.save()
+        return image
