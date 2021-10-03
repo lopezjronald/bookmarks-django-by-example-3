a462d73fd65dfa84c71a54ea37c3e336a6dbab56
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2021-10-02 19:34:56 -0700

    created ranking using redis. completed chapter 6

--- images/urls.py
+++ images/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'images'
 
 urlpatterns = [
-    path('create/', views.image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
+    path('create/', views.image_create, name='create'),
     path('like/', views.image_like, name='like'),
+    path('ranking/', views.image_ranking, name='ranking'),
     path('', views.image_list, name='list'),
 ]
